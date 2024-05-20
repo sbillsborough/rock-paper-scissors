88a02fb51fb533a9bcc818ba567cc7f308e7caeb
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-20 17:20:27 +0100

    Removed the input check function
    Removed the input validity check
    Added two prompts for players 1 and 2 that are stored in variables

--- JS/script.js
+++ JS/script.js
@@ -3,28 +3,8 @@
 // assign winning conditions with an if statement
 // alert the winner
 
-// User inputs and check if the input is valid
 let player1 = prompt("Player 1 choose rock, paper or scissors!");
-console.log(player1);
+let player2 = prompt("Player 2 choose rock, paper or scissors!");
 
-function playerOneChoice() {
-  if (player1 === "rock") {
-    alert("Player 1 chose: " + player1);
-    playerTwoChoice();
-  } else if (player1 === "paper") {
-    alert("Player 1 chose: " + player1);
-    playerTwoChoice();
-  } else if (player1 === "scissors") {
-    alert("Player 1 chose: " + player1);
-    playerTwoChoice();
-  } else {
-    player1 = prompt("INVALID INPUT! Player 1 choose rock, paper or scissors!");
-    playerOneChoice();
-  }
-}
-
-playerOneChoice();
-
-function playerTwoChoice() {
-  console.log("Player 2 choice!");
-}
+console.log(`player 1 choice ${player1}`);
+console.log(`player 2 choice ${player2}`);
