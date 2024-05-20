5a6036bdbe414680a46e79583a85783ccc67463f
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-20 17:41:14 +0100

    Comparison logic
    Added the logic to compare the rock, paper and scissors strings with ===
    Added the result to an alert

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,31 @@ let player2 = prompt("Player 2 choose rock, paper or scissors!");
 
 console.log(`player 1 choice ${player1}`);
 console.log(`player 2 choice ${player2}`);
+
+if (player1 === player2) {
+  alert(`Player 1 chose ${player1} and Player 2 chose ${player2}. DRAW!`);
+} else if (player1 === "rock" && player2 === "paper") {
+  alert(
+    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player two wins!`
+  );
+} else if (player1 === "rock" && player2 === "scissors") {
+  alert(
+    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player one wins!`
+  );
+} else if (player1 === "paper" && player2 === "rock") {
+  alert(
+    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player one wins!`
+  );
+} else if (player1 === "paper" && player2 === "scissors") {
+  `Player 1 chose ${player1} and Player 2 chose ${player2}. Player two wins!`;
+} else if (player1 === "scissors" && player2 === "rock") {
+  `Player 1 chose ${player1} and Player 2 chose ${player2}. Player two wins!`;
+} else if (player1 === "scissors" && player2 === "paper") {
+  alert(
+    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player one wins!`
+  );
+} else {
+  alert(
+    `Player 1 entered ${player1} and Player 2 entered ${player2}, please enter valid inputs!`
+  );
+}
