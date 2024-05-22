5645584266a364daa36bafe2291d1aad433a8ddf
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-22 17:19:33 +0100

    Add function to get the computers choice
    Added a getComputerChoice function with a default paramter of choice = 3
    Floor the choice and randomise it so the returned value ranges from 0 - 2
    console.log the returned value

--- JS/script.js
+++ JS/script.js
@@ -3,36 +3,8 @@
 // assign winning conditions with an if statement
 // alert the winner
 
-let player1 = prompt("Player 1 choose rock, paper or scissors!");
-let player2 = prompt("Player 2 choose rock, paper or scissors!");
-
-console.log(`player 1 choice ${player1}`);
-console.log(`player 2 choice ${player2}`);
-
-if (player1 === player2) {
-  alert(`Player 1 chose ${player1} and Player 2 chose ${player2}. DRAW!`);
-} else if (player1 === "rock" && player2 === "paper") {
-  alert(
-    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player two wins!`
-  );
-} else if (player1 === "rock" && player2 === "scissors") {
-  alert(
-    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player one wins!`
-  );
-} else if (player1 === "paper" && player2 === "rock") {
-  alert(
-    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player one wins!`
-  );
-} else if (player1 === "paper" && player2 === "scissors") {
-  `Player 1 chose ${player1} and Player 2 chose ${player2}. Player two wins!`;
-} else if (player1 === "scissors" && player2 === "rock") {
-  `Player 1 chose ${player1} and Player 2 chose ${player2}. Player two wins!`;
-} else if (player1 === "scissors" && player2 === "paper") {
-  alert(
-    `Player 1 chose ${player1} and Player 2 chose ${player2}. Player one wins!`
-  );
-} else {
-  alert(
-    `Player 1 entered ${player1} and Player 2 entered ${player2}, please enter valid inputs!`
-  );
+function getComputerChoice(choice = 3) {
+  return Math.floor(Math.random() * choice);
 }
+
+console.log(getComputerChoice());
