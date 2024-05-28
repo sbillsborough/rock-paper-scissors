e47389a690e57de65fe8111582e18e2164f3195c
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-28 11:32:00 +0100

    Play the game 5 times
    Added a letsPlay() function
    letsPlay() contains a for loop that runs playGame() 5 times
    if statement to compare the scores at the end of the 5 games

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
-// take in two user inputs with prompt
-// compare the two user inputs
-// assign winning conditions with an if statement
-// alert the winner
+// HUMAN CHOICE FUNCTION
+function getHumanChoice() {
+  return prompt("Player choose rock, paper or scissors!");
+}
 
-// COMPUTER CHOICE
+// COMPUTER CHOICE FUNCTION
 function getComputerChoice(choice = 3) {
   let computerNumber = Math.floor(Math.random() * choice);
   if (computerNumber === 0) {
@@ -15,27 +15,13 @@ function getComputerChoice(choice = 3) {
   }
 }
 
-// HUMAN CHOICE
-function getHumanChoice() {
-  return prompt("Player choose rock, paper or scissors!");
-}
-
-// getHumanChoice();
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
-
-// LOGIC TO PLAY THE ENTIRE GAME
-
-// SCORE GLOBAL VARIABLES
+// GLOBAL SCORE VARIABLES
 let humanScore = 0;
 let computerScore = 0;
 
+// LOGIC TO PLAY THE ENTIRE GAME
 function playGame() {
   // LOGIC TO PLAY A SINGLE ROUND
-
   function playRound(humanChoice, computerChoice) {
     // humanChoice = humanChoice.toLowerCase(); // variable reassignment
     console.log(humanChoice);
@@ -66,5 +52,19 @@ function playGame() {
   return playRound(getHumanChoice(), getComputerChoice());
 }
 
-playGame();
-playGame();
+// LOOP TO PLAY THE GAME 5 TIMES THEN COMPARE THE SCORES
+function letsPlay() {
+  for (let i = 0; i < 5; i++) {
+    playGame();
+  }
+
+  if (humanScore > computerScore) {
+    console.log("You won the game!");
+  } else if (computerScore > humanScore) {
+    console.log("The computer won the game!");
+  } else {
+    console.log("The game was tied!");
+  }
+}
+
+letsPlay();
