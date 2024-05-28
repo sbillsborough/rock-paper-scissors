3101741c2c7db462f63cfe6b8bc179c20e91b383
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2024-05-28 10:59:18 +0100

    Moved the playRound() function insode of the playGame() function
    Score is resetting to 0 each time the playGame() function is called
    getHumanChoice() prompt is calling three times

--- JS/script.js
+++ JS/script.js
@@ -22,41 +22,49 @@ function getHumanChoice() {
 
 // getHumanChoice();
 
-// SCORE GLOBAL VARIABLES
-let humanScore = 0;
-let computerScore = 0;
-
-// LOGIC TO PLAY A SINGLE ROUND
-
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase(); // variable reassignment
-  console.log(humanChoice);
-  console.log(computerChoice);
-  if (humanChoice === computerChoice) {
-    console.log("DRAW");
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("Computer wins! Paper beats Rock");
-    ++computerScore;
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win! Rock beats Scissors");
-    ++humanScore;
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win! Paper beats Rock");
-    ++humanScore;
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("Computer wins! Scissors beats paper");
-    ++computerScore;
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("Computer wins! Rock beats Scissors");
-    ++computerScore;
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win! Scissors beats Paper");
-    ++humanScore;
-  }
-  console.log(humanScore, computerScore);
-}
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+// playRound(humanSelection, computerSelection);
+
+// LOGIC TO PLAY THE ENTIRE GAME
+
+function playGame() {
+  // SCORE GLOBAL VARIABLES
+  let humanScore = 0;
+  let computerScore = 0;
+
+  // LOGIC TO PLAY A SINGLE ROUND
+
+  function playRound(humanChoice, computerChoice) {
+    // humanChoice = humanChoice.toLowerCase(); // variable reassignment
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if (humanChoice === computerChoice) {
+      console.log("DRAW");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("Computer wins! Paper beats Rock");
+      ++computerScore;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats Scissors");
+      ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats Rock");
+      ++humanScore;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("Computer wins! Scissors beats paper");
+      ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("Computer wins! Rock beats Scissors");
+      ++computerScore;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beats Paper");
+      ++humanScore;
+    }
+    console.log(humanScore, computerScore);
+  }
+  return playRound(getHumanChoice(), getComputerChoice());
+}
+
+playGame();
+playGame();
